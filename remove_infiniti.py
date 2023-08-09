5f848f2fa2f9a10515a7da5397e4ee16682551a5
Ivan Nguyen <ivan.h.nguyen@usace.army.mil>
2023-08-09 16:12:33 -0500

    hard code start and end date

--- remove_infiniti.py
+++ remove_infiniti.py
@@ -1,12 +1,8 @@
-# name=Remove Infiniti
-# displayinmenu=true
-# displaytouser=true
-# displayinselector=true
 '''
 Author: Ivan Nguyen
-Last Updated: 05-22-2023
+Last Updated: 08-09-2023
 Description: Remove Infiniti
-Version: 1.1
+Version: 1.4
 '''
 import java
 import time,calendar,datetime
@@ -17,15 +13,14 @@ import inspect
 import DBAPI
 import os
 import urllib
-from hec.heclib.util	            			import HecTime
-from hec.hecmath		            			import TimeSeriesMath
+from hec.heclib.util	            		import HecTime
+from hec.hecmath		            		import TimeSeriesMath
 from hec.io			                		import TimeSeriesContainer
-from rma.services		            			import ServiceLookup
-from java.util			            			import TimeZone
-from hec.data.tx                    			import QualityTx
-
-from java.text 						import SimpleDateFormat
-from java.util 						import Date
+from rma.services		            		import ServiceLookup
+from java.util			            		import TimeZone
+from hec.data.tx                    		import QualityTx
+from java.text 								import SimpleDateFormat
+from java.util 								import Date
 
 try:
 	StartTw  = datetime.datetime.now()
@@ -40,43 +35,29 @@ try:
 	print '='
 	
 
-	# set start time
-	#StartTw  = datetime.datetime.now() - ((datetime.timedelta(days=(9*365)-180)))
-    	#print 'StartTw = ' + str(StartTw)
-    	#StartTwStr = StartTw.strftime('%d%b%Y %H%M')
-    	#print 'StartTwStr = ' + StartTwStr
-
-	# set end time
-    	#EndTw  = datetime.datetime.now() - ((datetime.timedelta(days=(8*365)+100)))
-    	#print 'EndTw = ' + str(EndTw)
-    	#EndTwStr = EndTw.strftime('%d%b%Y %H%M')
-    	#print 'EndTwStr = ' + EndTwStr
-
 	# hard code start and end time window
-    	mysdate = '01Jan2015 0000'
+	mysdate = '01Jan2015 0000'
 	myedate = '31Dec2016 2400'
 
 	print 'mysdate = ' + mysdate
 	print 'myedate = ' + myedate
 
 	# connect to db
-        CwmsDb = DBAPI.open()
-        if not CwmsDb : raise Exception
-        #CwmsDb.setTimeWindow(StartTwStr, EndTwStr)
-        CwmsDb.setTimeWindow(mysdate,myedate)
-        CwmsDb.setOfficeId('MVS')
-        CwmsDb.setTimeZone('GMT')
-        CwmsDb.setStoreRule('Replace All')
+	CwmsDb = DBAPI.open()
+	if not CwmsDb : raise Exception
+	CwmsDb.setTimeWindow(mysdate,myedate)
+	CwmsDb.setOfficeId('MVS')
+	CwmsDb.setTimeZone('GMT')
+	CwmsDb.setStoreRule('Replace All')
 
-        # set tsc with start and end time
-        #Tsc = CwmsDb.get('Sullivan-Meramec.Elev.Inst.15Minutes.0.lrgsShef-rev', StartTwStr, EndTwStr)
-        Tsc = CwmsDb.get('Sullivan-Meramec.Stage.Inst.15Minutes.0.lrgsShef-rev', mysdate, myedate)
+	# set tsc with start and end time
+	Tsc = CwmsDb.get('Sullivan-Meramec.Stage.Inst.15Minutes.0.lrgsShef-rev', mysdate, myedate)
 
 
 	counter = 0
 	maxval = len(Tsc.values)
 
-        # loop through data set and print out the values
+	# loop through data set and print out the values
 	while counter < maxval:
 	
 		if Tsc.values[counter] > 1000000:
@@ -99,24 +80,13 @@ try:
 			
 			print 'value AFTER = ' + str(Tsc.values[counter])
 			print 'quality AFTER = ' + str(Tsc.quality[counter])
-			
-		#elif Tsc.values[counter] < -1000000:
-			#CwmsDb.setOverrideProtection(True)
-
-			# Remove protection flag in StageRev and Stage29
-			#Tsc.quality[counter] = QualityTx.clearProtected_int(Tsc.quality[counter])
-			
-			#print 'x = ' + str(Tsc.values[counter])
-			#Tsc.values[counter] =  -3.40282346639e+38
-			#Tsc.quality[counter] = 0
-			#print 'new x = ' + str(Tsc.values[counter])
 
 			print '==================================================='
 			
-       		counter += 1
-
-       	print '=== LOOP DONE ==='	
-       	#print 'Before DBPUT '
+			counter += 1
+			
+		print '=== LOOP DONE ==='	
+		#print 'Before DBPUT '
 	CwmsDb.put(Tsc)
 	#print 'After DBPUT '
 
@@ -132,11 +102,7 @@ try:
 	print '='
 	
 finally :
-	# close db cwms
-	try : CwmsDb.done()
-    	except : pass
-
-    	# close db connection
-    	try : conn.done()
-    	except : pass
-    	
+	# close the connection in the finally block
+	if CwmsDb:
+		CwmsDb.close()
+		print("Oracle database connection closed")
